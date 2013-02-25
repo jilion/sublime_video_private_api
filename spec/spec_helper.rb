215157ee9f2aa334955bdbcef3556aeec938bc18
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-02-25 14:25:11 +0100

    Configure RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,10 +10,14 @@ Rails.backtrace_cleaner.remove_silencers!
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run_including focus: true
   config.mock_with :rspec
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
-  config.order = "random"
+  config.fail_fast = ENV['FAST_FAIL'] != 'false'
+  config.order = ENV['ORDER'] || 'random'
 end
 
 require 'factory_girl_rails'
