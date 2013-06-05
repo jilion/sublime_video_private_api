fb5cecd889cf2fb4c3b919d0d1a9c3e5e89c10d8
Rémy Coutable <remy@rymai.me>
2013-06-05 16:43:07 +0200

    Handle records not found at this level

--- app/controllers/sublime_video_private_api_controller.rb
+++ app/controllers/sublime_video_private_api_controller.rb
@@ -3,8 +3,12 @@ require 'sublime_video_private_api/responders/pagination_headers_responder'
 
 class SublimeVideoPrivateApiController < ActionController::Base
   respond_to :json
+
   before_filter :authenticate
+
   responders SublimeVideoPrivateApi::Responders::PaginationHeadersResponder
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   private
 
@@ -12,5 +16,9 @@ class SublimeVideoPrivateApiController < ActionController::Base
     authenticate_or_request_with_http_token do |token, options|
       token == SublimeVideoPrivateApi.token
     end
+  end
+
+  def record_not_found
+    render request.format.ref => { error: 'Resource could not be found.' }, status: 404
   end
 end
