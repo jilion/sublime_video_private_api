5c4d03dfb9cad900335ab0e69d7cfcac2cdb708c
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-10-14 09:06:27 +0200

    Add Application.config.secret_key_base to Dummy App

--- spec/dummy/config/initializers/secret_token.rb
+++ spec/dummy/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '53471ce510e9beaf157e057926d0e312a36c230e3fa2176ba3249227619e169413ae7873387f9f7972e32c26e129a882acd3213879874243b66cd6884e73063a'
+Dummy::Application.config.secret_key_base = '53471ce510e9beaf157e057926d0e312a36c230e3fa2176ba3249227619e169413ae7873387f9f7972e32c26e129a882acd3213879874243b66cd6884e73063a'
